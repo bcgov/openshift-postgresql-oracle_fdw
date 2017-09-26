b2abd7c5ca5bf1cf565321845a3e3e73444bd40b
GeorgeWalker <george.walker@quartech.com>
2017-09-26 16:46:54 -0700

    Removed console output

--- root/usr/share/container-scripts/postgresql/common.sh
+++ root/usr/share/container-scripts/postgresql/common.sh
@@ -179,25 +179,7 @@ function create_users() {
 
 function create_fdw() {
   if [[ -v FDW_NAME && -v FDW_FOREIGN_SERVER && -v FDW_USER && -v FDW_PASS && -v FDW_FOREIGN_SCHEMA && -v FDW_SCHEMA ]]; then
-    echo Updating FDW settings...
-cat >&2 <<EOF
-	BEGIN;
-	CREATE EXTENSION IF NOT EXISTS oracle_fdw;   
-    DROP SERVER IF EXISTS ${FDW_NAME} CASCADE;
-    CREATE SERVER $FDW_NAME FOREIGN DATA WRAPPER oracle_fdw OPTIONS (dbserver '${FDW_FOREIGN_SERVER}');
-    DROP USER MAPPING IF EXISTS FOR public SERVER ${FDW_NAME};
-    CREATE USER MAPPING FOR public SERVER ${FDW_NAME} OPTIONS (user '${FDW_USER}', password '${FDW_PASS}');
-    DROP SCHEMA IF EXISTS ${FDW_SCHEMA};										
-    CREATE SCHEMA ${FDW_SCHEMA};
-    IMPORT FOREIGN SCHEMA "${FDW_FOREIGN_SCHEMA}" FROM SERVER ${FDW_NAME} INTO ${FDW_SCHEMA};
-    DROP ROLE IF EXISTS fdw_reader;
-    CREATE ROLE fdw_reader;
-    GRANT USAGE ON SCHEMA ${FDW_SCHEMA} TO fdw_reader;
-    GRANT SELECT ON ALL TABLES IN SCHEMA ${FDW_SCHEMA} TO fdw_reader;
-    GRANT fdw_reader to "${POSTGRESQL_USER}";
-	COMMIT;
-EOF
-	
+    echo Updating FDW settings...	
 	psql -d $POSTGRESQL_DATABASE <<EOF
 	BEGIN;
 	CREATE EXTENSION IF NOT EXISTS oracle_fdw;   
